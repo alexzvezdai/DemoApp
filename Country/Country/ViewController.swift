bdb812c7a97e6fc9c164195c1fe9dd11c2c60bc7
alexzvezdai <zvezdai74@ya.ru>
2019-11-30 22:51:07 +0300

    Изменил ReadMe и залил проект

---
+++ Country/Country/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Country
+//
+//  Created by Александр Звездай on 30.11.2019.
+//  Copyright © 2019 Александр Звездай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
